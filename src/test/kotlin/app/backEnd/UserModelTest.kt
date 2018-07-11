4cbca8d36a2d319da4e4f7a9ad012c9818128b6c
Acliu1498 <acliu1498@gmail.com>
2018-07-11 09:53:13 -0400

    changed some names and functionality

--- src/test/kotlin/app/backEnd/UserModelTest.kt
+++ src/test/kotlin/app/backEnd/UserModelTest.kt
@@ -3,45 +3,49 @@ package app.backEnd
 import org.junit.After
 import org.junit.Assert
 import org.junit.Test
+import persistance.TrDatabaseImpl
+import persistance.model.UserModel
 import java.io.File
 
 class UserModelTest {
-    val userHandler:UserHandler = UserHandler()
+    val trDatabaseImpl = TrDatabaseImpl()
+    val userRepo = trDatabaseImpl.getUserDao()
 
     @Test
     fun createTest() {
         var tmp = File("tr.db")
         Assert.assertTrue(tmp.exists())
-        userHandler.closeStore()
+        trDatabaseImpl.closeStore()
     }
 
     @Test(expected = NoSuchElementException::class)
     fun insertDeleteTest() {
         val hash = "213jlk21j3"
-        val tmp = userHandler.createUser(hash, "somewhere")
-        Assert.assertTrue(tmp._id > 0)
-        userHandler.deleteUser(hash)
-        userHandler.getUser(hash)
+        val tmp = userRepo.insert(hash, "somewhere")
+        Assert.assertTrue(tmp.id > 0)
+        userRepo.delete(tmp)
+        userRepo.getByHash(hash)
 
     }
 
     @Test
     fun getTest(){
+        val userList = ArrayList<UserModel>()
         for (i in 0..10){
-            userHandler.createUser(i.toString(), i.toString())
+            userList.add(userRepo.insert(i.toString(), i.toString()))
         }
         var num = 0
-        userHandler.getUsers().forEach {
+        userRepo.getAll().forEach {
             Assert.assertEquals(num.toString(), it.hash)
             num++
         }
-        for (i in 0..10){
-            userHandler.deleteUser(i.toString())
+        for (user in userList){
+            userRepo.delete(user = user)
         }
     }
 
     @After
     fun close(){
-        userHandler.closeStore()
+        trDatabaseImpl.closeStore()
     }
 }
