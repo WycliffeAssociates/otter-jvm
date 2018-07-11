2b5140b9f25d9b9c33b0c98c2fae5e00cb348008
Acliu1498 <acliu1498@gmail.com>
2018-07-11 11:37:15 -0400

    Created mappings, added rxKotlin dependencies and formatted user repo to implement Dao

--- src/main/kotlin/persistance/repo/UserRepo.kt
+++ src/main/kotlin/persistance/repo/UserRepo.kt
@@ -1,69 +1,71 @@
 package persistance.repo
 
+import io.reactivex.Completable
+import io.reactivex.Observable
 import io.requery.Persistable
 import io.requery.kotlin.eq
 import io.requery.sql.KotlinEntityDataStore
+import persistance.mapping.UserMapper
 import persistance.model.UserModel
 import persistance.model.UserModelEntity
 
-class UserRepo(dataStore: KotlinEntityDataStore<Persistable>){
+//todo implement DAO
+class UserRepo(dataStore: KotlinEntityDataStore<Persistable>): Dao<User>{
     private val dataStore = dataStore
+    private val userMapper = UserMapper()
     /**
      * function to create and insert a user into the database
      * takes in a hash and a path to a recording to creaete
      */
-    fun insert(hash:String, recordedNamePath: String): UserModel {
-        // attempts to find if user already exists
-        val tmp = UserModelEntity()
-        tmp.hash = hash
-        tmp.recordedNamePath = recordedNamePath
+    override fun insert(user: User): Observable<Int> {
         // returns created user
-        return dataStore.insert(tmp)
+        return Observable.just(dataStore.insert(userMapper.mapToEntity(user)).id)
     }
 
     /**
      * gets user by Id
      */
-    fun getById(id:Int){
-        return dataStore {
+    override fun getById(id:Int): Observable<User>{
+        val tmp = dataStore {
             val result = dataStore.select(UserModel::class).where(UserModel::id eq id)
             result.get().first()
         }
+        return Observable.just(userMapper.mapFromEntity(tmp))
     }
-
 
     /**
      * given a hash gets the user
      */
-    fun getByHash(hash: String): UserModel {
-        return dataStore {
+    fun getByHash(hash: String): Observable<User> {
+        val tmp = dataStore {
             val result = dataStore.select(UserModel::class).where(UserModel::hash eq hash)
             result.get().first()
         }
+        return Observable.just(userMapper.mapFromEntity(tmp))
     }
 
     /**
      * gets all the users currently stored in db
      */
-    fun getAll(): List<UserModel>{
-        return dataStore {
+    override fun getAll(): Observable<List<User>>{
+        val tmp = dataStore {
             val result = dataStore.select(UserModel::class)
-            result.get().toList()
+            result.get().asIterable()
         }
+        return Observable.fromIterable(tmp.map { User(it.id, it.hash, it.recordedNamePath) })
+
     }
 
-
-    fun updateUser(hash: String, newPath: String){
-        return dataStore {
-            //todo figure out
-        }
+    fun updateUser(hash: String, newPath: String): Completable{
+        //todo figure out
     }
 
     /**
      * deletes user by id
      */
-    fun delete(user: UserModel){
-        dataStore.delete(UserModel::class).where(UserModel::id eq user.id).get().value()
+    fun delete(user: User): Completable{
+        return Completable.fromAction{
+            dataStore.delete(UserModel::class).where(UserModel::id eq user.id).get().value()
+        }
     }
-
 }
