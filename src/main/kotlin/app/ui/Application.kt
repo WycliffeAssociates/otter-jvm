adc96133806f34f6a1a1c77d187b01edfdca4081
Matthew Russell <mbr4477@gmail.com>
2018-07-20 10:13:57 -0400

    Removed unnecessary Application.kt imports

--- src/main/kotlin/app/ui/Application.kt
+++ src/main/kotlin/app/ui/Application.kt
@@ -1,12 +1,5 @@
 package app.ui
 
-import data.DayNight
-import data.Language
-import data.User
-import data.UserPreferences
-import persistence.injection.DaggerDatabaseComponent
-import java.util.*
-
 object Application {
     @JvmStatic
     fun main(args: Array<String>) {
