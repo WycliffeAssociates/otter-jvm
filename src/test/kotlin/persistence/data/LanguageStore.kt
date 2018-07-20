d34ff8947226f3006dc666fb352fc8a1f3ef2dce
Jennifer <jjhuang1@andrew.cmu.edu>
2018-07-20 13:57:13 -0400

    pulls from door43

--- src/test/kotlin/persistence/data/LanguageStore.kt
+++ src/test/kotlin/persistence/data/LanguageStore.kt
@@ -4,13 +4,20 @@ import data.Language
 
 class LanguageStore {
     companion object {
-        val languages: List<Language> = listOf(
+        /*val languages: List<Language> = listOf(
                 Language(1, "en", "English", true, "English"),
                 Language(2, "es", "Espanol", true, "Spanish"),
                 Language(3, "fr", "Français", false, "French"),
                 Language(4, "cmn", "官话", true, "Mandarin Chinese"),
                 Language(5, "ar", "العَرَبِيَّة", false, "Arabic")
-        )
+        )*/
+        val door43Retrieval = Door43Retrieval()
+        val languages: List<Language> = door43Retrieval.getLanguages()
+        fun hi(){
+            for (lang in languages){
+                println(lang.name)
+            }
+        }
 
         fun getLanguageForSlug(slug: String) : Language {
             return languages.filter { it.slug == slug }.first()
