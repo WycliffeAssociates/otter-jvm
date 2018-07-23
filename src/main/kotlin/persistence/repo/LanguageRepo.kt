2e599ce49c2c12c171b902c436da5db4dc3fc275
Matthew Russell <mbr4477@gmail.com>
2018-07-23 11:15:00 -0400

    Added observables to subscribeOn Schedulers.io thread

--- src/main/kotlin/persistence/repo/LanguageRepo.kt
+++ src/main/kotlin/persistence/repo/LanguageRepo.kt
@@ -4,6 +4,7 @@ import data.Language
 import data.dao.Dao
 import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
 import io.requery.Persistable
 import io.requery.kotlin.eq
 import io.requery.sql.KotlinEntityDataStore
@@ -12,36 +13,47 @@ import persistence.model.ILanguageEntity
 import persistence.model.IUserLanguage
 
 
-class LanguageRepo constructor(private val dataStore: KotlinEntityDataStore<Persistable>): Dao<Language> {
-    val languageMapper = LanguageMapper()
+class LanguageRepo(private val dataStore: KotlinEntityDataStore<Persistable>): Dao<Language> {
+    private val languageMapper = LanguageMapper()
     /**
      * given a language deletes the entry within the table
      */
     override fun delete(language: Language): Completable {
         return Completable.fromAction {
-            dataStore.delete(IUserLanguage::class).where(IUserLanguage::languageEntityid eq language.id).get().value()
-            dataStore.delete(ILanguageEntity::class).where(ILanguageEntity::id eq language.id).get().value()
-        }
+            dataStore.delete(IUserLanguage::class)
+                        .where(IUserLanguage::languageEntityid eq language.id)
+                        .get().value()
+            dataStore.delete(ILanguageEntity::class)
+                        .where(ILanguageEntity::id eq language.id)
+                        .get().value()
+        }.subscribeOn(Schedulers.io())
     }
 
     /**
      * gets all language entries and returns them as an observable language
      */
     override fun getAll(): Observable<List<Language>> {
-        val languageList = dataStore.select(ILanguageEntity::class).get().toList()
-
-        return Observable.just(languageList.map { languageMapper.mapFromEntity(it) })
+        return Observable.create<List<ILanguageEntity>> {
+            it.onNext(dataStore.select(ILanguageEntity::class)
+                    .get().toList())
+        }.subscribeOn(Schedulers.io())
+        .map {
+            it.map { languageMapper.mapFromEntity(it) }
+        }
     }
 
     /**
      *  given an id gets and return a language observable
      */
     override fun getById(id: Int): Observable<Language> {
-        val language = dataStore.invoke {
-            val result = dataStore.select(ILanguageEntity::class).where(ILanguageEntity::id eq id)
-            result.get().first()
+        return Observable.create<ILanguageEntity> {
+            it.onNext(dataStore.select(ILanguageEntity::class)
+                    .where(ILanguageEntity::id eq id)
+                    .get().first())
+        }.subscribeOn(Schedulers.io())
+        .map {
+            languageMapper.mapFromEntity(it)
         }
-        return Observable.just(languageMapper.mapFromEntity(language))
     }
 
     /**
@@ -49,7 +61,9 @@ class LanguageRepo constructor(private val dataStore: KotlinEntityDataStore<Pers
      * and returns the generated id as an observable
      */
     override fun insert(language: Language): Observable<Int> {
-        return Observable.just(dataStore.insert(languageMapper.mapToEntity(language)).id)
+        return Observable.create<Int> {
+            it.onNext(dataStore.insert(languageMapper.mapToEntity(language)).id)
+        }.subscribeOn(Schedulers.io())
     }
 
     /**
@@ -59,7 +73,7 @@ class LanguageRepo constructor(private val dataStore: KotlinEntityDataStore<Pers
     override fun update(language: Language): Completable {
         return Completable.fromAction {
             dataStore.update(languageMapper.mapToEntity(language))
-        }
+        }.subscribeOn(Schedulers.io())
     }
 
     /**
@@ -67,11 +81,14 @@ class LanguageRepo constructor(private val dataStore: KotlinEntityDataStore<Pers
      * as an observable list of languages
      */
     fun getGatewayLanguages(): Observable<List<Language>> {
-        val languageList = dataStore.select(ILanguageEntity::class).where(ILanguageEntity::gateway eq true)
-                .get()
-                .toList()
-                .asIterable()
-
-        return Observable.just(languageList.map { languageMapper.mapFromEntity(it) })
+        return Observable.create<List<ILanguageEntity>> {
+            it.onNext(dataStore.select(ILanguageEntity::class)
+                    .where(ILanguageEntity::gateway eq true)
+                    .get().toList())
+        }
+        .subscribeOn(Schedulers.io())
+        .map {
+            it.map { languageMapper.mapFromEntity(it) }
+        }
     }
 }
