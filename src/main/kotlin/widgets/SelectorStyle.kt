9e33539bd761fd409683ebd6ff1c389a30154d47
Caleb Benedick <cbenedick2014@my.fit.edu>
2018-07-26 09:52:00 -0400

    removed language from the selection widget and set up an interface with a language to interface adapter

--- src/main/kotlin/widgets/SelectorStyle.kt
+++ src/main/kotlin/widgets/SelectorStyle.kt
@@ -3,6 +3,7 @@ package widgets
 import javafx.geometry.Pos
 import javafx.scene.paint.Color
 import javafx.scene.text.FontWeight
+import recources.UIColors
 import tornadofx.*
 
 /**
@@ -58,7 +59,7 @@ class SelectorStyle : Stylesheet() {
                     backgroundColor = multi(Color.TRANSPARENT)
                     opacity = 0.65
                     alignment = Pos.CENTER_RIGHT
-                    padding = box(11.px, 10.px, 5.px, 10.px)
+                    padding = box(12.px, 10.px, 5.px, 10.px)
 
                     and(hover) {
                         opacity = 1.0
