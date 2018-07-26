0c5bfef4f486436d144cf4910beb55c82514c528
joyastockwell <stockwja@rose-hulman.edu>
2018-07-26 09:26:43 -0400

    Added documentation to api folder

--- src/main/kotlin/api/Config.kt
+++ src/main/kotlin/api/Config.kt
@@ -6,23 +6,34 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.*
 
+// Gets objects from JSON (all of which are languages)
+// As of now, the JSON is  a list of languages and we turn them into objects and keep them in a door43api object
 class Config {
+    // Will store all our language objects and provide getters
     private val door43Api: Door43Api
 
+    // Gets location of resource folder
     private val locale = Locale.getDefault()
     private val labels = ResourceBundle.getBundle("Resources", locale)
 
     init {
+        // Configures retrofit
         val retrofit = Retrofit.Builder()
+                // Gets base url for where to retrieve json
                 .baseUrl(labels.getString("base_json"))
+                // Turns json into objects using moshi
                 .addConverterFactory(MoshiConverterFactory.create())
+                // We need rx because the objects it turns into will be observables
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                // Tells computer to actually do the above
                 .build()
 
+        // Data object that contains the objects just built
         door43Api = retrofit.create(Door43Api::class.java)
 
     }
 
+    // Gets a language from the objects just built
     fun getLang(lang: String): Observable<List<Door43Language>> {
         return door43Api.getLanguage(lang)
     }
--- src/main/kotlin/api/Door43Api.kt
+++ src/main/kotlin/api/Door43Api.kt
@@ -5,6 +5,10 @@ import io.reactivex.Observable
 
 interface Door43Api {
 
+    // Extension added to the base URL of the json
     @GET("/exports/langnames.json")
+    // Finds all places ln occurs in the JSON
+    // (ln is the language's name, used to separate the languages because each language has one)
+    // Makes a new POJO every time it sees ln
     fun getLanguage(@Query("ln")name: String): Observable<List<Door43Language>>
 }
