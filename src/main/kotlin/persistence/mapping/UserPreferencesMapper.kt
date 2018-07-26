8c3584e22ea5026a861bba3be9406be8728dbbc3
Jennifer <jjhuang1@andrew.cmu.edu>
2018-07-26 14:17:04 -0400

    updated UserPreferencesMapper

--- src/main/kotlin/persistence/mapping/UserPreferencesMapper.kt
+++ src/main/kotlin/persistence/mapping/UserPreferencesMapper.kt
@@ -4,24 +4,21 @@ import data.model.Language
 import data.model.UserPreferences
 import data.dao.Dao
 import data.mapping.Mapper
+import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
 import persistence.model.IUserPreferencesEntity
 import persistence.model.UserPreferencesEntity
-class UserPreferencesMapper(private val languageRepo: Dao<Language>):
-        Mapper<IUserPreferencesEntity, UserPreferences> {
 
-    override fun mapFromEntity(type: IUserPreferencesEntity): UserPreferences {
+class UserPreferencesMapper(private val languageRepo: Dao<Language>):
+        Mapper<IUserPreferencesEntity, Observable<UserPreferences>> {
+
+    override fun mapFromEntity(type: IUserPreferencesEntity): Observable<UserPreferences> {
         // gets from database and maps preferred source and target language
         val preferredSourceLanguage = languageRepo.getById(type.sourceLanguageId)
-                                                  .blockingFirst()
         val preferredTargetLanguage = languageRepo.getById(type.targetLanguageId)
-                                                  .blockingFirst()
-
-        return UserPreferences(
-                type.id,
-                preferredSourceLanguage,
-                preferredTargetLanguage
-        )
+        return Observable.zip(preferredSourceLanguage, preferredTargetLanguage,
+                BiFunction<Language, Language, UserPreferences>{a, b -> UserPreferences(type.id, a, b)})
     }
 
     override fun mapToEntity(type: UserPreferences): IUserPreferencesEntity {
