933984d82d4eb9649cd95830ebd2004df46a0a72
joyastockwell <stockwja@rose-hulman.edu>
2018-07-27 10:33:42 -0400

    All in Kotlin; function plays sample audio file

--- src/main/kotlin/app/ui/Main.kt
+++ src/main/kotlin/app/ui/Main.kt
@@ -1,11 +1,15 @@
-import api.ui.AudioMine
+import javafx.scene.media.AudioClip
 import java.io.File
 
 fun main(args: Array<String>) {
     val file = File("WycliffeTest.wav")
-    val audio = AudioMine(file.toURI().toString())
-    audio.play()
+    playKotlin(file.toURI().toString());
 
     // Audio playing happens on another thread so we need to sleep Main's thread for long enough for it to play
     Thread.sleep(10000)
+}
+
+fun playKotlin(str: String) {
+    val myClip = AudioClip(str)
+    myClip.play();
 }
