5a3a1883ad81354ba555603906e75331a3e6ffdd
Acliu1498 <acliu1498@gmail.com>
2018-07-27 13:54:48 -0400

    updated AppDatabaseImpl for jooq

--- src/main/kotlin/persistence/injection/DatabaseModule.kt
+++ src/main/kotlin/persistence/injection/DatabaseModule.kt
@@ -1,12 +1,12 @@
-//package persistence.injection
-//
-//import dagger.Module
-//import dagger.Provides
-//import data.persistence.AppDatabase
-//import persistence.AppDatabaseImpl
-//
-//@Module
-//class DatabaseModule {
-//    @Provides
-//    fun providesAppDatabase() : AppDatabase = AppDatabaseImpl
-//}
+package persistence.injection
+
+import dagger.Module
+import dagger.Provides
+import data.persistence.AppDatabase
+import persistence.AppDatabaseImpl
+
+@Module
+class DatabaseModule {
+    @Provides
+    fun providesAppDatabase() : AppDatabase = AppDatabaseImpl
+}
