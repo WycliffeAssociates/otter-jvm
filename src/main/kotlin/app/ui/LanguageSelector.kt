80817d4e941ed52119fe6e2ca7f1e098f1433da5
Caleb Benedick <cbenedick2014@my.fit.edu>
2018-07-30 13:44:52 -0400

    moved functionality of the combobox into a view on the app end, and made some changes oin how styling is done.

---
+++ src/main/kotlin/app/ui/LanguageSelector.kt
@@ -0,0 +1,124 @@
+package app.ui
+
+import data.model.Language
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.PublishSubject
+import javafx.geometry.Insets
+import javafx.geometry.Pos
+import javafx.scene.layout.Priority
+import javafx.scene.layout.VBox
+import javafx.scene.paint.Color
+import tornadofx.*
+import widgets.*
+import java.util.ResourceBundle
+
+/**
+ * This class is used to make allow a user to select items from a filterable list
+ * and select which of those that are selected as the preferred, or default, item.
+ */
+
+class LanguageSelector(
+        languages: List<Language>,
+        label: String,
+        hint: String,
+        colorResourcesFile: String,
+        private val colorAccent: Color,
+        private val updateLanguages: PublishSubject<Language>,
+        private val preferredLanguage: PublishSubject<Language>
+) : View() {
+
+    private val selectionData : List<ComboBoxSelectionItem>
+    private val compositeDisposable : CompositeDisposable
+    private val chips : MutableList<Chip>
+    private val viewModel : LanguageSelectorViewModel
+    private val comboBoxSelector : ComboBoxSelector
+
+    override val root = VBox()
+
+
+    init {
+        messages = ResourceBundle.getBundle(colorResourcesFile)
+
+        compositeDisposable = CompositeDisposable()
+        selectionData = languages.map { LanguageSelectionItem(it) }
+        chips = mutableListOf()
+        viewModel = LanguageSelectorViewModel(updateLanguages, preferredLanguage, languages)
+        comboBoxSelector = ComboBoxSelector(selectionData, hint, viewModel::addNewValue)
+
+
+        with(root) {
+
+            alignment = Pos.CENTER
+
+            label(label)
+            add(comboBoxSelector)
+            comboBoxSelector.style {
+                focusColor = colorAccent
+                borderColor = multi(box(colorAccent))
+            }
+            separator()
+
+            flowpane {
+
+                /** Redraw the flowpane with any new data */
+                compositeDisposable.add(
+                        updateLanguages.subscribe {
+                            val language = it
+                            val check = chips.map { it.labelText == language.toTextView() }
+
+                            if (check.contains(true)) {
+                                chips.removeAt(check.indexOf(true))
+                            } else {
+                                chips.add(0,
+                                        Chip(
+                                                language.toTextView(),
+                                                colorAccent,
+                                                viewModel::removeLanguage,
+                                                viewModel::newPreferredlanguage
+                                        )
+                                )
+                            }
+
+                            this.requestFocus()
+                            children.clear()
+                            children.addAll(chips)
+                        }
+                )
+
+                /** Change the chip colors based on which one is selected */
+                compositeDisposable.add(
+                        preferredLanguage.subscribe {
+                            newSelected(it.toTextView())
+                        }
+                )
+
+                vgrow = Priority.ALWAYS
+                hgap = 6.0
+                vgap = 6.0
+            }
+
+            padding = Insets(40.0)
+            spacing = 10.0
+        }
+    }
+
+    /** Change the highlighted chip to the one most recently clicked */
+    private fun newSelected(language: String) {
+        chips.first().requestFocus()
+        for (chip in chips) {
+            if (chip.labelText == language) {
+                chip.label.textFill = c(messages["UI_NEUTRAL"])
+                chip.button.fill = colorAccent
+            } else {
+                chip.label.textFill = c(messages["UI_NEUTRAL_TEXT"])
+                chip.button.fill = c(messages["UI_NEUTRAL"])
+            }
+        }
+    }
+
+    /** Dispose of disposables */
+    override fun onUndock() {
+        super.onUndock()
+        compositeDisposable.clear()
+    }
+}
--- src/main/kotlin/widgets/Chip.kt
+++ src/main/kotlin/widgets/Chip.kt
@@ -1,5 +1,7 @@
 package widgets
 
+import de.jensd.fx.glyphs.materialicons.MaterialIcon
+import de.jensd.fx.glyphs.materialicons.MaterialIconView
 import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.effect.DropShadow
@@ -8,8 +10,6 @@ import javafx.scene.layout.HBox
 import javafx.scene.layout.StackPane
 import javafx.scene.paint.Color
 import javafx.scene.shape.Rectangle
-import de.jensd.fx.glyphs.materialicons.MaterialIcon
-import de.jensd.fx.glyphs.materialicons.MaterialIconView
 import tornadofx.*
 
 /**
@@ -19,61 +19,49 @@ import tornadofx.*
  * stackpane for what we want to access.
  */
 
-class Chip(val labelText : String,
-           fillColor : Color,
-           textColor : Color,
-           onDelete : (Chip) -> Unit,
-           onClick : (Chip) -> Unit
+class Chip(
+        val labelText: String,
+        dropShadowColor : Color,
+        onDelete : (Chip) -> Unit,
+        onClick : (Chip) -> Unit
 ) : StackPane() {
 
     val label : Label
     val deleteButton : Button
     val button : Rectangle
-    val chip : StackPane
 
     init {
 
-        label = label(labelText) {
-            textFill = textColor
-
-        }
+        label = label(labelText)
 
         deleteButton = button {
-            val icon = MaterialIconView(MaterialIcon.CLEAR, "20px")
-            icon.fillProperty().bind(label.textFillProperty())
-
-            add(icon)
+            val deleteIcon = MaterialIconView(MaterialIcon.CLEAR, "20px")
+            deleteIcon.fillProperty().bind(label.textFillProperty())
+            add(deleteIcon)
             action {
                 onDelete(this@Chip)
             }
         }
 
         button = rectangle {
-            fill = fillColor
             height = 25.0
 
             // bind the width to the size of the text in the label
             widthProperty().bind(label.widthProperty() + deleteButton.widthProperty())
         }
 
-        chip = stackpane {
 
-            setOnMouseEntered {
-                effect = DropShadow(5.0, fillColor)
-            }
-            setOnMouseExited {
-                effect = null
-            }
-
-            add(button)
-
-            add(HBox(label, deleteButton))
-
-            //addClass(chipStyle)
-            addEventFilter(MouseEvent.MOUSE_CLICKED) { onClick(this@Chip) }
-
+        setOnMouseEntered {
+            effect = DropShadow(5.0, dropShadowColor)
+        }
+        setOnMouseExited {
+            effect = null
         }
 
+        add(button)
+        add(HBox(label, deleteButton))
+
+        addEventFilter(MouseEvent.MOUSE_CLICKED) { onClick(this) }
     }
 
 }
