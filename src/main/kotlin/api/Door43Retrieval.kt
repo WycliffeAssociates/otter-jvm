53a29d24c88b74d4c3d1119b1a43cd9766073f0d
Jennifer <jjhuang1@andrew.cmu.edu>
2018-07-31 13:19:26 -0400

    function renaming

--- src/main/kotlin/api/Door43Retrieval.kt
+++ src/main/kotlin/api/Door43Retrieval.kt
@@ -9,11 +9,11 @@ class Door43Retrieval(val retrofitConfig: RetrofitConfig = RetrofitConfig()) {
     val appDatabase = DaggerDatabaseComponent.builder().build().inject()
     val languageDao = appDatabase.getLanguageDao()
 
-    fun getLanguages() : Observable<List<Door43Language>> = retrofitConfig.getLang()
+    fun getAllLanguages() : Observable<List<Door43Language>> = retrofitConfig.getLangs()
 
     fun getAllAndInsert() {
         val door43Mapper = Door43Mapper
-        getLanguages().map {
+        getAllLanguages().map {
             it.forEach {
                 val language = door43Mapper.mapToLanguage(it)
                 languageDao.insert(language)
