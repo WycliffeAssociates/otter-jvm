d5e2cb64b06597fba2334f56f62215f067d25407
Jennifer <jjhuang1@andrew.cmu.edu>
2018-07-31 09:48:45 -0400

    utility function for inserting langs into database

--- src/main/kotlin/api/Door43Retrieval.kt
+++ src/main/kotlin/api/Door43Retrieval.kt
@@ -1,8 +1,24 @@
 package api
 
+import api.model.Door43Mapper
 import io.reactivex.Observable
+import persistence.injection.DaggerDatabaseComponent
 
 class Door43Retrieval(val retrofitConfig: RetrofitConfig = RetrofitConfig()) {
 
+    val appDatabase = DaggerDatabaseComponent.builder().build().inject()
+    val languageDao = appDatabase.getLanguageDao()
+
     fun getLanguages() : Observable<List<Door43Language>> = retrofitConfig.getLang("")
+
+    fun getAllAndInsert() {
+        val door43Mapper = Door43Mapper
+        getLanguages().map {
+            it.forEach {
+                val language = door43Mapper.mapToLanguage(it)
+                languageDao.insert(language)
+            }
+        }
+    }
+
 }
