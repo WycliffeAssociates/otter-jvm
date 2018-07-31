3d9b6af8b4e24cd1cfc66dbbdab2558659291e36
Matthew Russell <mbr4477@gmail.com>
2018-07-31 11:34:20 -0400

    Rename to AppPreferencesImpl (#127)

--- src/main/kotlin/persistence/AppPreferencesImpl.kt
+++ src/main/kotlin/persistence/AppPreferencesImpl.kt
@@ -4,9 +4,9 @@ import data.persistence.AppPreferences
 import java.util.prefs.Preferences
 
 // preferences object that stores user-independent preference data
-class JavaAppPreferences : AppPreferences {
+class AppPreferencesImpl : AppPreferences {
     private val CURRENT_USER_ID_KEY = "currentUserId"
-    private val preferences = Preferences.userNodeForPackage(JavaAppPreferences::class.java)
+    private val preferences = Preferences.userNodeForPackage(AppPreferencesImpl::class.java)
 
     override fun getCurrentUserId(): Int {
         return preferences.getInt(CURRENT_USER_ID_KEY, 0)
