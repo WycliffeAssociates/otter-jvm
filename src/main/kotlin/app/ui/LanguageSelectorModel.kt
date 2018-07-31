2e0d8f01f729e8abe82953cfad21d194a2966f95
Caleb Benedick <cbenedick2014@my.fit.edu>
2018-07-31 14:59:36 -0400

    Changed test file names, the renamed the combobox widget to FilterableComboBox, and added in depth comments for each class

--- src/main/kotlin/app/ui/LanguageSelectorModel.kt
+++ src/main/kotlin/app/ui/LanguageSelectorModel.kt
@@ -3,6 +3,11 @@ package app.ui
 import data.model.Language
 import widgets.ComboBoxSelectionItem
 
+/**
+ * This class holds a list of selected languages and tracks which one is the preferred language.
+ *
+ * @author Caleb Benedick
+ */
 class LanguageSelectorModel(private val languages : List<Language>) {
     var preferredSelection : Language? = null
     val selectedData : MutableList<Language> = mutableListOf()
