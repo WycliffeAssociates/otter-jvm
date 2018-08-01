b5b0f9eedebd6bf1157b3f1bb9be408d9a0fd196
Caleb Benedick <cbenedick2014@my.fit.edu>
2018-08-01 15:57:56 -0400

    cleaner UIColorsManager for swapping bundles

--- src/main/kotlin/UIColorsManager.kt
+++ src/main/kotlin/UIColorsManager.kt
@@ -1,8 +1,17 @@
 
+import javafx.beans.property.SimpleObjectProperty
 import javafx.beans.property.SimpleStringProperty
+import tornadofx.onChange
 import java.util.*
 
 object UIColorsManager {
     val colorResourceFile = SimpleStringProperty("UIColors")
-    var Colors = ResourceBundle.getBundle(colorResourceFile.value)
+    val Colors = SimpleObjectProperty<ResourceBundle>()
+
+    init {
+        Colors.value = ResourceBundle.getBundle(colorResourceFile.value)
+        colorResourceFile.onChange {
+            Colors.value = ResourceBundle.getBundle(it)
+        }
+    }
 }
