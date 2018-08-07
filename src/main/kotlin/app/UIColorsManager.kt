cc7abc3402d59ab157503287feb219e164e1a4be
Caleb Benedick <cbenedick2014@my.fit.edu>
2018-08-07 12:00:41 -0400

    Removed UIColorManager and replaced all uses of it with UIColorObject

--- src/main/kotlin/app/UIColorsManager.kt
+++
@@ -1,8 +0,0 @@
-package app
-import javafx.beans.property.SimpleStringProperty
-import java.util.*
-
-object UIColorsManager {
-    val colorResourceFile = SimpleStringProperty("UIColors")
-    var Colors = ResourceBundle.getBundle(colorResourceFile.value)
-}
