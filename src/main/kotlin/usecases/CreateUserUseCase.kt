3f17fc29d514dd6d62b251027e8522d952628aa3
Matthew Russell <mbr4477@gmail.com>
2018-08-08 09:35:45 -0400

    Dev use case create user (#162)
    * began creating a create user use case
    
    * began creating a create user use case
    
    * began creating a create user use case
    
    * creating test
    
    * tests
    
    * gradle stuff
    
    * adding audio stuff
    
    * finishing up tests

---
+++ src/main/kotlin/usecases/CreateUserUseCase.kt
@@ -0,0 +1,197 @@
+package usecases
+
+import data.model.Language
+import data.model.User
+import data.model.UserPreferences
+import device.audio.injection.DaggerAudioComponent
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import org.apache.commons.codec.binary.Hex
+import org.apache.commons.codec.digest.DigestUtils
+import persistence.DirectoryProvider
+import persistence.injection.DaggerPersistenceComponent
+import persistence.injection.PersistenceComponent
+import persistence.injection.PersistenceModule
+import tornadofx.toProperty
+import java.io.ByteArrayInputStream
+import java.io.File
+import java.io.FileInputStream
+import javax.sound.sampled.AudioFileFormat
+import javax.sound.sampled.AudioFormat
+import javax.sound.sampled.AudioInputStream
+import javax.sound.sampled.AudioSystem
+
+class CreateUserUseCase {
+    private var currentRecording: File? = null
+    private var currentImage: File? = null
+    private var audioHash = ""
+    private val sourceLanguages: MutableList<Language> = mutableListOf()
+    private val targetLanguages: MutableList<Language> = mutableListOf()
+    private var preferredSource: Language? = null
+    private var preferredTarget: Language? = null
+    private val audioBytes = emptyList<Byte>().toMutableList()
+    private val databaseComponent = DaggerPersistenceComponent
+        .builder()
+        .build()
+        .injectDatabase()
+    private val directoryProvider = DaggerPersistenceComponent
+        .builder()
+        .build()
+        .injectDirectoryProvider()
+    private val audioRecorder = DaggerAudioComponent
+        .builder()
+        .build()
+        .injectRecorder()
+    private val audioPlayer = DaggerAudioComponent
+        .builder()
+        .build()
+        .injectPlayer()
+
+    init {
+        audioRecorder
+            .getAudioStream()
+            .subscribe {
+                audioBytes.addAll(it.toList())
+            }
+    }
+
+    // TODO: implement
+    fun startRecording() {
+        currentRecording?.delete()
+        audioBytes.clear()
+        audioRecorder.start()
+    }
+
+    fun stopRecording() {
+        audioRecorder.stop()
+        currentRecording = createTempFile()
+        // writes the bytes to the temp file
+        val audioStream = AudioInputStream(ByteArrayInputStream(
+            audioBytes.toByteArray()),
+            AudioFormat(44100F,16,1,true,false),
+            audioBytes.size.toLong()
+        )
+        AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE, currentRecording)
+        generateHash()
+    }
+
+    fun playRecording() {
+        audioPlayer.load(currentRecording?: throw NullPointerException("Recording has not been set"))
+        audioPlayer.play()
+    }
+
+    fun setImage(file: File) {
+        currentImage?.delete()
+        currentImage = file
+    }
+
+    fun setImageWithIdenticon(){
+        // ensures that something has been recorded to generate identicon
+        if (currentRecording == null){
+            throw NullPointerException("Recording has not been set")
+        }
+        currentImage?.delete()
+        currentImage = File(directoryProvider.userProfileImageDirectory, "$audioHash.svg")
+        currentImage
+            ?.printWriter()
+            .use {
+                it?.println(audioHash)
+            }
+    }
+
+    private fun generateHash(): String {
+        audioHash = String(Hex.encodeHex(DigestUtils.md5(FileInputStream(currentRecording))))
+        return audioHash
+    }
+
+    fun getImage(): File = currentImage?: throw NullPointerException("Image Has not been set")
+
+    fun getLanguages(): Observable<List<Language>> {
+        return databaseComponent.getLanguageDao().getAll()
+    }
+
+    fun addSourceLanguage(language: Language) {
+        sourceLanguages.add(language)
+    }
+
+    fun removeSourceLanguage(language: Language){
+        sourceLanguages.remove(language)
+        // updates preferred source
+        if (preferredSource == language){
+            preferredSource = null
+        }
+    }
+
+    fun addTargetLanguage(language: Language) {
+        targetLanguages.add(language)
+    }
+
+    fun removeTargetLanguage(language: Language){
+        targetLanguages.remove(language)
+        // updates preferred target
+        if(preferredTarget == language){
+            preferredTarget = null
+        }
+    }
+
+    fun setPreferredSource(language: Language) {
+        // enforces constraint that preferred must be within list of languages
+        if (!sourceLanguages.contains(language)) {
+            throw NoSuchElementException("Preferred source language does not exist in list of source languages")
+        }
+        preferredSource = language
+    }
+
+    fun setPreferredTarget(language: Language) {
+        if (!targetLanguages.contains(language)) {
+            throw NoSuchElementException("Preferred target language does not exist in list of target languages")
+        }
+        preferredTarget = language
+    }
+
+    fun commit(): Observable<User> {
+        val user = User(
+            id = 0,
+            audioHash = audioHash,
+            audioPath = commitRecording(),
+            imagePath = currentImage?.path ?: getImage().path,
+            sourceLanguages = sourceLanguages,
+            targetLanguages = targetLanguages,
+            userPreferences = UserPreferences(
+                id = 0,
+                sourceLanguage = preferredSource ?:
+                throw NullPointerException("No preferred Source has been selected"),
+                targetLanguage = preferredTarget ?:
+                throw NullPointerException("No preferred Target has been selected")
+            )
+        )
+
+        val userDao = databaseComponent.getUserDao()
+
+        return Observable.create<User> {
+            user.id = userDao.insert(user).blockingFirst()
+            it.onNext(user)
+        }.subscribeOn(Schedulers.io())
+    }
+
+    /**
+     * helper function that converts
+     * the temp recording file to an actual one
+     * and returns the path of that file
+     */
+    private fun commitRecording(): String{
+        val commitAudio: File
+
+        if(currentRecording != null) {
+            commitAudio = File(directoryProvider.userProfileAudioDirectory, "$audioHash.wav")
+            commitAudio
+                .printWriter()
+                .print(currentRecording?.readText())
+            currentRecording?.delete()
+        } else {
+            throw NullPointerException("No recording has been set")
+        }
+
+        return commitAudio.path
+    }
+}
---
+++ src/test/kotlin/persistence/usecases/CreateUserUseCaseTest.kt
@@ -0,0 +1,120 @@
+package persistence.usecases
+
+import data.model.Language
+import data.model.User
+import data.model.UserPreferences
+import device.audio.AudioPlayer
+import device.audio.AudioRecorderImpl
+import device.audio.injection.DaggerAudioComponent
+import io.reactivex.Observable
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.powermock.api.mockito.PowerMockito
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+import persistence.data.LanguageStore
+import persistence.injection.DaggerPersistenceComponent
+import persistence.repo.UserRepo
+import usecases.CreateUserUseCase
+
+@RunWith(PowerMockRunner::class)
+@PrepareForTest(DaggerPersistenceComponent::class, DaggerAudioComponent::class)
+class CreateUserUseCaseTest {
+    private val mockPersistenceBuilder = Mockito.mock(
+        DaggerPersistenceComponent.Builder::class.java,
+        Mockito.RETURNS_DEEP_STUBS)
+    private val mockUserDao = Mockito.mock(UserRepo::class.java, Mockito.RETURNS_DEEP_STUBS)
+    // TODO: mock svg generator
+
+    @Before
+    fun setup() {
+        PowerMockito.mockStatic(DaggerPersistenceComponent::class.java)
+        Mockito
+            .`when`(DaggerPersistenceComponent.builder())
+            .thenReturn(mockPersistenceBuilder)
+        Mockito
+            .`when`(mockPersistenceBuilder
+                .build()
+                .injectDatabase()
+                .getUserDao()
+            ).thenReturn(mockUserDao)
+
+        // TODO: mock svg generation
+    }
+
+    @Test(expected = NullPointerException::class)
+    fun testIfCommitFailsWithoutRecording(){
+        val createUseCase = CreateUserUseCase()
+
+        createUseCase.setImageWithIdenticon()
+        createUseCase.addSourceLanguage(LanguageStore.getById(1))
+        createUseCase.addTargetLanguage(LanguageStore.getById(2))
+        createUseCase.setPreferredSource(LanguageStore.getById(1))
+        createUseCase.setPreferredTarget(LanguageStore.getById(2))
+
+        createUseCase.commit()
+    }
+
+    @Test(expected = NoSuchElementException::class)
+    fun testIfSettingPreferredSourceFailsIfSourceNotInSourceLanguages(){
+        val createUseCase = CreateUserUseCase()
+
+        createUseCase.addSourceLanguage(LanguageStore.getById(1))
+        createUseCase.setPreferredSource(LanguageStore.getById(3))
+    }
+
+    @Test(expected = NoSuchElementException::class)
+    fun testIfSettingPreferredSourceFailsIfTargetNotInTargetLanguages(){
+        val createUseCase = CreateUserUseCase()
+
+        createUseCase.addTargetLanguage(LanguageStore.getById(2))
+        createUseCase.setPreferredSource(LanguageStore.getById(3))
+    }
+
+    @Test(expected = NullPointerException::class)
+    fun testIfGetImageFailsIfImageNotSet(){
+        val createUseCase = CreateUserUseCase()
+
+        createUseCase.getImage()
+    }
+
+    @Test(expected = NullPointerException::class)
+    fun testIfSetImageFailsIfRecordingNotSet(){
+        val createUseCase = CreateUserUseCase()
+        createUseCase.setImageWithIdenticon()
+    }
+
+    @Test
+    fun testIfIdenticonIsGeneratedFromHash(){}
+
+    @Test(expected = NullPointerException::class)
+    fun testIfCommitFailsIfPreferredSourceNotSet(){
+        val createUseCase = CreateUserUseCase()
+
+        createUseCase.startRecording()
+        createUseCase.stopRecording()
+        createUseCase.setImageWithIdenticon()
+        createUseCase.addSourceLanguage(LanguageStore.getById(1))
+        createUseCase.addTargetLanguage(LanguageStore.getById(2))
+        createUseCase.setPreferredTarget(LanguageStore.getById(2))
+
+        createUseCase.commit()
+    }
+
+    @Test(expected = NullPointerException::class)
+    fun testIfCommitFailsIfPreferredTargetNotSet(){
+        val createUseCase = CreateUserUseCase()
+
+        createUseCase.startRecording()
+        createUseCase.stopRecording()
+        createUseCase.setImageWithIdenticon()
+        createUseCase.addSourceLanguage(LanguageStore.getById(1))
+        createUseCase.addTargetLanguage(LanguageStore.getById(2))
+        createUseCase.setPreferredSource(LanguageStore.getById(2))
+
+        createUseCase.commit()
+    }
+}
