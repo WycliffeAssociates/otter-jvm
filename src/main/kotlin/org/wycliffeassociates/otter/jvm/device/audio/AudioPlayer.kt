00b59412aa6bac81a0a924e45faf346e2d5dd2d1
Matthew Russell <mbr4477@gmail.com>
2018-09-14 16:44:23 -0400

    Remove extra overrides in AudioPlayer

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/device/audio/AudioPlayer.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/device/audio/AudioPlayer.kt
@@ -10,14 +10,6 @@ import org.wycliffeassociates.otter.common.device.IAudioPlayer
 import org.wycliffeassociates.otter.common.device.IAudioPlayerListener
 
 class AudioPlayer: IAudioPlayer {
-    override fun addEventListener(onEvent: (event: AudioPlayerEvent) -> Unit) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun addEventListener(listener: IAudioPlayerListener) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
     private var clip: Clip = AudioSystem.getClip()
 
     override fun addEventListener(onEvent: (event: AudioPlayerEvent) -> Unit) {
