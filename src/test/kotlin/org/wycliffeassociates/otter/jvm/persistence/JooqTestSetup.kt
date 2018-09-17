d84b5fc748386d0daa71912d67deef5fadeb64ef
Matthew Russell <mbr4477@gmail.com>
2018-09-17 10:03:50 -0400

    Added db test helpers

---
+++ src/test/kotlin/org/wycliffeassociates/otter/jvm/persistence/JooqTestSetup.kt
@@ -0,0 +1,37 @@
+package org.wycliffeassociates.otter.jvm.persistence
+
+import org.jooq.Configuration
+import org.jooq.SQLDialect
+import org.jooq.impl.DSL
+import org.sqlite.SQLiteDataSource
+import java.io.File
+
+object JooqTestSetup {
+    fun setup(databasePath: String): Configuration {
+        Class.forName("org.sqlite.JDBC")
+        println("Creating $databasePath")
+        val sqLiteDataSource = SQLiteDataSource()
+        sqLiteDataSource.url = "jdbc:sqlite:$databasePath"
+        sqLiteDataSource.config.toProperties().setProperty("foreign_keys", "true")
+        val config = DSL.using(sqLiteDataSource, SQLDialect.SQLITE).configuration()
+        val file = File(listOf("src", "main", "resources", "createAppDb.sql").joinToString(File.separator))
+        val sql = StringBuffer()
+        file.forEachLine {
+            sql.append(it)
+            if (it.contains(";")) {
+                config.dsl().fetch(sql.toString())
+                sql.delete(0, sql.length)
+            }
+        }
+        return config
+    }
+
+    fun tearDown(databasePath: String) {
+        // delete existing database
+        val dbFile = File(databasePath)
+        if (dbFile.exists()) {
+            println("Deleting $databasePath")
+            dbFile.delete()
+        }
+    }
+}
