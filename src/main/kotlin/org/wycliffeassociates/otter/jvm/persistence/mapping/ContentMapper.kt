8b5bb4d35ca4c0ffebe23638b4b965a04dc56dfa
Matthew Russell <mbr4477@gmail.com>
2018-09-18 19:55:49 -0400

    Mr content take daos (#215)
    * Create first content mapper and test
    
    * Start take mapper
    
    * Small updates

---
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/persistence/mapping/ContentMapper.kt
@@ -0,0 +1,40 @@
+package org.wycliffeassociates.otter.jvm.persistence.mapping
+
+import io.reactivex.Observable
+import jooq.tables.pojos.ContentEntity
+import org.wycliffeassociates.otter.common.data.mapping.Mapper
+import org.wycliffeassociates.otter.common.data.model.Chunk
+
+class ContentMapper : Mapper<Observable<ContentEntity>, Observable<Chunk>> {
+    override fun mapFromEntity(type: Observable<ContentEntity>): Observable<Chunk> {
+        return type
+                .map {
+                    Chunk(
+                            it.id,
+                            it.sort,
+                            it.label,
+                            it.start,
+                            // end: make a call to derived dao to figure out max derived verse
+                            // if not derived, then same as start
+                            it.start,
+                            // Make call to Take dao to get this, if it exists
+                            null
+                    )
+                }
+    }
+
+    override fun mapToEntity(type: Observable<Chunk>): Observable<ContentEntity> {
+        return type
+                .map {
+                    ContentEntity(
+                            it.id,
+                            null, // Leave null to be filled in by dao
+                            it.labelKey,
+                            it.selectedTake?.id,
+                            it.start,
+                            it.sort
+                    )
+                }
+    }
+
+}
