049eddd75fcba4c4337a91df74e0ea525870b6fe
Nathan Shanko <natibekele@gmail.com>
2018-09-18 16:57:57 -0400

    add views for wizard

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/projectcreation/view/fragments/SelectBook.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/projectcreation/view/fragments/SelectBook.kt
@@ -1,9 +1,19 @@
 package org.wycliffeassociates.otter.jvm.app.ui.projectcreation.view.fragments
 
+import org.wycliffeassociates.otter.common.data.model.Project
+import org.wycliffeassociates.otter.jvm.app.widgets.ProjectCard
 import tornadofx.*
-class SelectBook: Fragment() {
-
-    override val root = datagrid(listOf("0")){
-
-    }
-}
+//class SelectBook: Fragment() {
+//
+//    override val root = DataGrid<Project>()
+//    init{
+//        with(root) {
+//            items=viewModel.projects
+//            cellCache {
+//                projectcard(it) {
+//                    loadButton.text = messages["create"]
+//                }
+//            }
+//        }
+//    }
+//}
--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/projectcreation/view/fragments/SelectLanguage.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/projectcreation/view/fragments/SelectLanguage.kt
@@ -1,2 +1,44 @@
 package org.wycliffeassociates.otter.jvm.app.ui.projectcreation.view.fragments
 
+import org.wycliffeassociates.otter.common.data.model.Language
+import org.wycliffeassociates.otter.jvm.app.ui.languageSelectorFragment.LanguageSelectionItem
+import org.wycliffeassociates.otter.jvm.app.widgets.filterableComboBox.ComboBoxSelectionItem
+import org.wycliffeassociates.otter.jvm.app.widgets.filterableComboBox.FilterableComboBox
+import tornadofx.*
+import java.util.*
+
+class SelectLanguage: Fragment() {
+    val model = SelectLanguageModel()
+    val selectionData: List<ComboBoxSelectionItem>
+    init {
+        selectionData = model.languageVals.map{LanguageSelectionItem(it)}
+    }
+    override  val root =  anchorpane {
+        hbox{
+            this+= FilterableComboBox(selectionData,"this is your hint", ::println)
+            this+= FilterableComboBox(selectionData,"this is your hint", ::println)
+        }
+
+        button("Confirm"){
+            anchorpaneConstraints {
+                rightAnchor = 30.0
+                bottomAnchor = 50.0
+            }
+        }
+    }
+}
+
+class SelectLanguageModel {
+    val languageVals = listOf(
+            Language(0, "ENG", "English", true, "0"),
+            Language(0, "MAN", "Mandarin", true, "1"),
+            Language(0, "ESP", "Spanish", true, "2"),
+            Language(0, "ARA", "Arabic", true, "3"),
+            Language(0, "RUS", "Russian", true, "4"),
+            Language(0, "AAR", "Afrikaans", true, "5"),
+            Language(0, "HEB", "Hebrew", true, "6"),
+            Language(0, "JAP", "Japanese", true, "7"),
+            Language(0, "FRN", "French", true, "8")
+    )
+
+}
