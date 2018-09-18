9427ca6f9f8590c83dc83f4103d96d1bf3668da0
Nathan Shanko <natibekele@gmail.com>
2018-09-18 16:57:57 -0400

    add views for wizard

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/projectcreation/view/fragments/SelectTestament.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/projectcreation/view/fragments/SelectTestament.kt
@@ -1,2 +1,25 @@
 package org.wycliffeassociates.otter.jvm.app.ui.projectcreation.view.fragments
 
+import de.jensd.fx.glyphs.materialicons.MaterialIcon
+import de.jensd.fx.glyphs.materialicons.MaterialIconView
+import org.wycliffeassociates.otter.jvm.app.ui.imageLoader
+import tornadofx.*
+import java.io.File
+
+class SelectTestament: Fragment() {
+    override  val root = hbox(20) {
+        button{
+            vbox{
+                this += imageLoader(File("Users/NathanShanko/Downloads/Old Testament.svg"))
+                label("Old Testament")
+            }
+        }
+
+        button {
+            vbox {
+                this += imageLoader(File("Users/NathanShanko/Downloads/Cross.svg"))
+                label("New Testament")
+            }
+        }
+    }
+}
