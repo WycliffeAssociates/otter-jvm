4c3bf35dc448fd01c7f89ebd30255ce0b2c53b76
Matthew Russell <mbr4477@gmail.com>
2018-09-24 16:56:37 -0400

    Updated chunk mapper for finding end

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/persistence/mapping/ChunkMapper.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/persistence/mapping/ChunkMapper.kt
@@ -3,12 +3,18 @@ package org.wycliffeassociates.otter.jvm.persistence.mapping
 import io.reactivex.Maybe
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
+import jooq.tables.daos.ContentDerivativeDao
+import jooq.tables.daos.ContentEntityDao
 import jooq.tables.pojos.ContentEntity
 import org.wycliffeassociates.otter.common.data.mapping.Mapper
 import org.wycliffeassociates.otter.common.data.model.Chunk
 import org.wycliffeassociates.otter.jvm.persistence.repo.TakeDao
 
-class ChunkMapper(private val takeDao: TakeDao) : Mapper<Single<ContentEntity>, Maybe<Chunk>> {
+class ChunkMapper(
+        private val takeDao: TakeDao,
+        private val derivedDao: ContentDerivativeDao,
+        private val contentEntityDao: ContentEntityDao
+) : Mapper<Single<ContentEntity>, Maybe<Chunk>> {
     override fun mapFromEntity(type: Single<ContentEntity>): Maybe<Chunk> {
         return type
                 .toMaybe()
@@ -17,13 +23,20 @@ class ChunkMapper(private val takeDao: TakeDao) : Mapper<Single<ContentEntity>, 
                             entity.sort,
                             entity.label,
                             entity.start,
-                            // end: make a call to derived dao to figure out max derived verse
-                            // if not derived, then same as start
                             entity.start,
                             // Make call to Take dao to get this, if it exists
                             null,
                             entity.id
                     )
+                    // Set the chunk's end
+                    chunk.end = derivedDao
+                            // Get the derivation table rows for this chunk
+                            .fetchByContentFk(chunk.id)
+                            // Get the corresponding content from the content table
+                            .map { contentEntityDao.fetchOneById(it.sourceFk).start }
+                            // Find the largest source start
+                            .max() ?: entity.start
+
                     if (entity.selectedTakeFk != null) {
                         // Selected take exists
                         takeDao
