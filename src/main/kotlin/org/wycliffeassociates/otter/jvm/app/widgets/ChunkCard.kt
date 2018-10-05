dae12237effcb766d619c4b6f860d6a93fe9f4eb
Matthew Russell <mbr4477@gmail.com>
2018-10-05 16:10:17 -0400

    Chunk card styling

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/widgets/ChunkCard.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/widgets/ChunkCard.kt
@@ -3,6 +3,7 @@ package org.wycliffeassociates.otter.jvm.app.widgets
 
 import com.jfoenix.controls.JFXButton
 import javafx.geometry.Pos
+import javafx.scene.layout.Priority
 import javafx.scene.layout.VBox
 import org.wycliffeassociates.otter.common.data.model.Chunk
 import tornadofx.*
@@ -16,9 +17,17 @@ class ChunkCard(val chunk: Chunk) : VBox() {
             alignment = Pos.CENTER
             spacing = 10.0
             // TODO: Localization
-            label(chunk.labelKey)
+            label(chunk.labelKey) {
+                vgrow = Priority.ALWAYS
+                maxHeight = Double.MAX_VALUE
+                addClass("title")
+            }
             chunk.selectedTake?.let {
-                label("Take ${it.number}")
+                label("Take ${it.number}") {
+                    vgrow = Priority.ALWAYS
+                    maxHeight = Double.MAX_VALUE
+                    addClass("selected-take")
+                }
             }
             add(actionButton)
         }
