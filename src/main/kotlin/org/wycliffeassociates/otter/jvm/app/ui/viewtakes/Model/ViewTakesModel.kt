12dd12ea9a6b1215af8b8d9814207cfa0c939a3f
Matthew Russell <mbr4477@gmail.com>
2018-10-05 08:15:24 -0400

    Pull in view takes screen code
    - Checkout from ns_view_takes (25843b1f77b453f4e8a3e0dfc9c84e9fd21e468e) to avoid merge conflicts

---
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/viewtakes/Model/ViewTakesModel.kt
@@ -0,0 +1,75 @@
+package org.wycliffeassociates.otter.jvm.app.ui.viewtakes.Model
+
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
+import javafx.scene.Node
+import javafx.scene.shape.Rectangle
+import org.wycliffeassociates.otter.jvm.app.widgets.Take
+import tornadofx.*
+import java.io.File
+import java.util.*
+
+class ViewTakesModel() {
+
+    val takes = listOf(
+            Take(
+                    1,
+                    Calendar.Builder().setDate(2018, 8, 8).build().time,
+                    File("/Users/nathanshanko/Documents/test1.wav"),
+                    true
+            ),
+            Take(
+                    2,
+                    Calendar.Builder().setDate(2018, 9, 9).build().time,
+                    File("/Users/nathanshanko/Documents/test1.wav"),
+                    false
+            ),
+            Take(
+                    3,
+                    Calendar.Builder().setDate(2018, 9, 18).build().time,
+                    File("/Users/nathanshanko/Documents/test1.wav"),
+                    true
+            ),
+            Take(
+                    4,
+                    Calendar.Builder().setDate(2018, 10, 12).build().time,
+                    File("/Users/nathanshanko/Documents/test1.wav"),
+                    false
+            ),
+            Take(
+                    12,
+                    Calendar.Builder().setDate(2018, 10, 13).build().time,
+                    File("/Users/nathanshanko/Documents/test1.wav"),
+                    true
+            ),
+            Take(
+                    21,
+                    Calendar.Builder().setDate(2018, 10, 14).build().time,
+                    File("/Users/nathanshanko/Documents/test1.wav"),
+                    false
+            )
+
+    )
+
+    var alteranteTakes: ObservableList<Take> by property(
+            FXCollections.observableList(takes.toMutableList()))
+    val alternateTakesProperty = getProperty(ViewTakesModel::alteranteTakes)
+
+    var selectedTake: Node by property()
+    var selectedTakeProperty = getProperty(ViewTakesModel::selectedTake)
+
+    var takeToCompare: Node by property()
+    var takeToCompareProperty = getProperty(ViewTakesModel::takeToCompare)
+
+    var comparingTake: Boolean by property(false)
+    var comparingTakeProperty = getProperty(ViewTakesModel::comparingTake)
+
+    var draggingTake: Boolean by property(false)
+    var draggingTakeProperty = getProperty(ViewTakesModel::draggingTake)
+
+    var draggingShadow: Node by property (Rectangle())
+    var draggingShadowProperty = getProperty(ViewTakesModel::draggingShadow)
+
+
+}
+
---
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/viewtakes/ViewModel/ViewTakesViewModel.kt
@@ -0,0 +1,93 @@
+package org.wycliffeassociates.otter.jvm.app.ui.viewtakes.ViewModel
+
+
+import javafx.collections.FXCollections
+import javafx.scene.Node
+import javafx.scene.input.MouseEvent
+import javafx.scene.layout.VBox
+import org.wycliffeassociates.otter.jvm.app.ui.inject.Injector
+import org.wycliffeassociates.otter.jvm.app.ui.viewtakes.Model.ViewTakesModel
+import org.wycliffeassociates.otter.jvm.app.widgets.TakeCard
+
+import tornadofx.*
+
+class ViewTakesViewModel : ViewModel() {
+    val model = ViewTakesModel()
+
+    var selectedTakeProperty = bind {model.selectedTakeProperty}
+    val alternateTakesProperty = bind { model.alternateTakesProperty }
+
+    var takeItems = mutableListOf<TakeCard>()
+    var takeToCompareProperty = bind{model.takeToCompareProperty}
+
+    var comparingTakeProperty = bind {model.comparingTakeProperty}
+    var draggingShadowProperty = bind{model.draggingShadowProperty}
+    var draggingTakeProperty = bind {model.draggingTakeProperty}
+
+    var mousePosition : DoubleArray by property(doubleArrayOf(0.0,0.0))
+    var mousePositionProperty = getProperty(ViewTakesViewModel::mousePosition)
+
+    var takeCards = FXCollections.observableArrayList<TakeCard>()
+//    var takeCardsProperty = getProperty(ViewTakesViewModel::takeCards)
+
+    var tempId: Int = 0
+    var tempTargetNode: Node = VBox()
+    var tempTakeCardTarget : Node = VBox()
+    lateinit var tempTakeCard: TakeCard
+
+    init {
+        alternateTakesProperty.value.forEach {
+            takeCards.add(TakeCard(it, Injector.audioPlayer))
+        }
+    }
+
+    fun startDrag(evt: MouseEvent) {
+        if (model.comparingTake == false && model.draggingTake == false) {
+           tempTargetNode = evt.target as Node
+             tempTakeCardTarget = tempTargetNode.findParentOfType(TakeCard::class) as Node
+             tempTakeCard = tempTargetNode.findParentOfType(TakeCard::class) as TakeCard
+
+            if(tempTakeCardTarget != model.draggingShadow) {
+                        model.draggingShadow = tempTakeCardTarget
+                    }
+                    takeItems.firstOrNull()
+                    .apply {
+                        model.draggingTake = true
+                    }
+        }
+    }
+
+    fun cancelDrag(evt: MouseEvent) {
+        model.draggingTake = false
+    }
+
+    fun animateDrag(evt: MouseEvent) {
+        if (model.comparingTake == false) {
+           mousePosition = doubleArrayOf(evt.sceneX,evt.sceneY)
+        }
+    }
+
+    fun completeDrag(evt: MouseEvent) {
+
+        if (model.selectedTake == null) {
+            model.selectedTake = tempTakeCardTarget
+            model.draggingTake = false
+        } else {
+            model.takeToCompare = tempTakeCardTarget
+            model.comparingTake = true
+            model.draggingTake = false
+        }
+    }
+
+    fun setTake() {
+        model.takeToCompare.removeFromParent()
+        model.selectedTake = model.takeToCompare
+        model.comparingTake = false
+    }
+
+    fun cancelSetTake() {
+        model.comparingTake = false
+        model.takeToCompare.removeFromParent()
+    }
+
+}
