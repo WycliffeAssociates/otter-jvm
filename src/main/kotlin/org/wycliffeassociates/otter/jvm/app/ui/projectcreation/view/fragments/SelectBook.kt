ee3e0dda7c6c41efb936efd1e13dfb1768147e2e
Matthew Russell <mbr4477@gmail.com>
2018-10-11 10:14:35 -0400

    JFX and other project card/buttons styling

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/projectcreation/view/fragments/SelectBook.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/projectcreation/view/fragments/SelectBook.kt
@@ -1,10 +1,9 @@
 package org.wycliffeassociates.otter.jvm.app.ui.projectcreation.view.fragments
 
 import javafx.geometry.Insets
+import de.jensd.fx.glyphs.materialicons.MaterialIcon
+import de.jensd.fx.glyphs.materialicons.MaterialIconView
 import javafx.geometry.Pos
-import javafx.scene.effect.DropShadow
-import javafx.scene.paint.Color
-import org.wycliffeassociates.otter.jvm.app.UIColorsObject.Colors
 import org.wycliffeassociates.otter.jvm.app.ui.projectcreation.viewmodel.ProjectCreationViewModel
 import org.wycliffeassociates.otter.jvm.app.ui.projecthome.ProjectHomeView
 import org.wycliffeassociates.otter.jvm.app.ui.styles.AppStyles
@@ -26,28 +25,17 @@ class SelectBook : View() {
                     alignment = Pos.CENTER
                     padding = Insets(10.0)
                     bindChildren(viewModel.bookList) {
-                        hbox {
-                            projectcard(it) {
-                                style {
-                                    prefHeight = 250.0.px
-                                    prefWidth = 232.0.px
-                                    backgroundColor += c(Colors["base"])
-                                    backgroundRadius += box(10.0.px)
-                                    borderRadius += box(10.0.px)
-                                    effect = DropShadow(4.0, 2.0, 4.0, Color.GRAY)
-                                }
-                                buttonText = messages["create"]
-                                cardButton.apply {
-                                    addClass(AppStyles.cardButton)
-                                    style {
-                                        effect = DropShadow(0.0, Color.TRANSPARENT)
-                                        backgroundColor += c(Colors["primary"])
-                                        textFill = c(Colors["base"])
-                                    }
-                                    action {
-                                        viewModel.selectedBookProperty.value = it
-                                        onSave()
-                                    }
+                        projectcard(it) {
+                            addClass(AppStyles.projectCard)
+                            graphicContainer.apply {
+                                addClass(AppStyles.projectGraphicContainer)
+                                add(MaterialIconView(MaterialIcon.IMAGE, "75px"))
+                            }
+                            cardButton.apply {
+                                text = messages["create"]
+                                action {
+                                    viewModel.selectedBookProperty.setValue(it)
+                                    onSave()
                                 }
                             }
                         }
