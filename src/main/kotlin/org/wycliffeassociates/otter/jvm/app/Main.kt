8da8aca2eeff601039018b6022641e187e84ed12
Nathan Shanko <natibekele@gmail.com>
2018-11-05 15:10:23 -0500

    remove local in main

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/Main.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/Main.kt
@@ -12,7 +12,6 @@ import org.wycliffeassociates.otter.jvm.app.ui.styles.AppStyles
 import org.wycliffeassociates.otter.jvm.app.ui.viewtakes.view.ViewTakesStylesheet
 import tornadofx.*
 import java.io.File
-import java.util.*
 
 class MyApp : App(Workspace::class) {
     init {
@@ -22,7 +21,6 @@ class MyApp : App(Workspace::class) {
         importStylesheet(AppStyles::class)
         workspace.header.removeFromParent()
         workspace.add(MainMenu())
-        FX.locale = Locale.FRANCE
     }
     override fun onBeforeShow(view:UIComponent) {
         workspace.dock<ProjectHomeView>()
