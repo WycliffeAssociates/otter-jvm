0c83087e78871098e3a6e2d0ca81523d1d21b119
Matthew Russell <mbr4477@gmail.com>
2018-11-09 09:45:31 -0500

    Edited logging info

--- init_common.sh
+++ init_common.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Get the currently checked out branch
-git branch --list
 jvm_branch=$BRANCH
 echo "Detected 8woc2018-jvm on $jvm_branch"
 # Check if branch exists in common
 pushd ../8woc2018-common
-echo "Looking for origin/$jvm_branch in 8woc2018-common"
+echo "Looking for origin/$jvm_branch in 8woc2018-common..."
 git rev-parse --verify origin/$jvm_branch
 if [ $? -eq 0 ]; then
     # Branch exists
