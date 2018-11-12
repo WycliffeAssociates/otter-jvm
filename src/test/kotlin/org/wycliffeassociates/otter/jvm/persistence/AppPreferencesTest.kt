f72bca87b0d1ba403c0e360f792d4291ca79f50c
Matthew Russell <mbr4477@gmail.com>
2018-11-12 16:47:31 -0500

    Update app preferences tests

--- src/test/kotlin/org/wycliffeassociates/otter/jvm/persistence/AppPreferencesTest.kt
+++ src/test/kotlin/org/wycliffeassociates/otter/jvm/persistence/AppPreferencesTest.kt
@@ -6,25 +6,70 @@ import org.junit.Test
 import java.util.prefs.Preferences
 
 class AppPreferencesTest {
+    val appPreferences = AppPreferences
 
     @Test
-    fun testIfPutGetCurrentUserIdPutsAndGetsCorrectInfo() {
+    fun shouldSetAndGetUserId() {
         val input = 5
-        val expected = input
+        val expected = 5
 
-        val appPreferences = AppPreferences
         appPreferences.setCurrentUserId(input)
         val result = appPreferences.currentUserId()
         Assert.assertEquals(expected, result)
     }
 
     @Test
-    fun testIfGetCurrentUserIdWhenNoExistingIdReturnsNull() {
+    fun shouldReturnNullIfNoUserId() {
         val expected = null
+        val result = appPreferences.getCurrentUserId()
+        Assert.assertEquals(expected, result)
+    }
 
-        val appPreferences = AppPreferences
-        val result = appPreferences.currentUserId()
+    @Test
+    fun shouldSetAndGetAppInit() {
+        val input = true
+        val expected = true
+        appPreferences.setAppInitialized(input)
+        val result = appPreferences.getAppInitialized()
+        Assert.assertEquals(expected, result)
+    }
 
+    @Test
+    fun shouldReturnFalseIfNoInit() {
+        val expected = false
+        val result = appPreferences.getAppInitialized()
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun shouldSetAndGetEditorId() {
+        val input = 1
+        val expected = 1
+        appPreferences.setEditorPluginId(input)
+        val result = appPreferences.getEditorPluginId()
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun shouldReturnNullIfNoEditorId() {
+        val expected = null
+        val result = appPreferences.getEditorPluginId()
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun shouldSetAndGetRecorderId() {
+        val input = 1
+        val expected = 1
+        appPreferences.setRecorderPluginId(input)
+        val result = appPreferences.getRecorderPluginId()
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun shouldReturnNullIfNoRecorderId() {
+        val expected = null
+        val result = appPreferences.getRecorderPluginId()
         Assert.assertEquals(expected, result)
     }
 
