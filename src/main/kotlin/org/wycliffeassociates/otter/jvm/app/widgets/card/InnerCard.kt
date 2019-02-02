b55ee276ec990f86db22ec3b02304739222a74ef
Nathan Shanko <natibekele@gmail.com>
2019-02-02 12:53:34 -0500

    edit inner card to add images

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/widgets/card/InnerCard.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/widgets/card/InnerCard.kt
@@ -2,12 +2,14 @@ package org.wycliffeassociates.otter.jvm.app.widgets.card
 
 import javafx.beans.property.SimpleStringProperty
 import javafx.geometry.Pos
+import javafx.scene.Node
 import javafx.scene.effect.GaussianBlur
 import javafx.scene.layout.VBox
 import javafx.scene.paint.Color
+import org.wycliffeassociates.otter.jvm.app.images.ImageLoader
 import tornadofx.*
 
-class InnerCard : VBox() {
+class InnerCard(cardGraphic: Node? = null) : VBox() {
     val titleProperty = SimpleStringProperty()
     var title by titleProperty
 
@@ -23,51 +25,60 @@ class InnerCard : VBox() {
 
     init {
         importStylesheet<DefaultStyles>()
-
-        alignment = Pos.BOTTOM_CENTER
         stackpane {
-            ellipse {
-                centerX = 0.0
-                centerY = 0.0
-                radiusX = 50.0
-                radiusY = 10.0
-                fill = Color.WHITE
-                effect = GaussianBlur(15.0)
-                visibleProperty().bind(titleProperty.booleanBinding{it!= null})
+            if(cardGraphic != null) {
+            add(cardGraphic).apply {
+                style{
+                    backgroundColor+= Color.LIGHTGRAY
+                }
+            }}
+            vbox {
+                alignment = Pos.BOTTOM_CENTER
+                stackpane {
+                    ellipse {
+                        centerX = 0.0
+                        centerY = 0.0
+                        radiusX = 50.0
+                        radiusY = 10.0
+                        fill = Color.WHITE
+                        effect = GaussianBlur(15.0)
+                        visibleProperty().bind(titleProperty.booleanBinding { it != null })
 
-            }
-            label(titleProperty) {
-                addClass(DefaultStyles.defaultTitle)
+                    }
+                    label(titleProperty) {
+                        addClass(DefaultStyles.defaultTitle)
+                    }
+                }
+                stackpane {
+                    ellipse {
+                        centerX = 50.0
+                        centerY = 0.0
+                        radiusX = 20.0
+                        radiusY = 20.0
+                        fill = Color.WHITE
+                        effect = GaussianBlur(15.0)
+                        visibleProperty().bind(bodyTextProperty.booleanBinding { it != null })
+                    }
+                    label(bodyTextProperty) {
+                        addClass(DefaultStyles.defaultBody)
+                    }
+                }
+                label(majorLabelProperty) {
+                    addClass(DefaultStyles.defaultMajorLabel)
+                }
+                label(minorLabelProperty) {
+                    addClass(DefaultStyles.defaultMinorLabel)
+                }
+                progressbar(0.2) { addClass(DefaultStyles.defaultCardProgressBar) }
+                addClass(DefaultStyles.defaultInnerCard)
             }
         }
-        stackpane {
-            ellipse {
-                centerX = 50.0
-                centerY = 0.0
-                radiusX = 20.0
-                radiusY = 20.0
-                fill = Color.WHITE
-                effect = GaussianBlur(15.0)
-                visibleProperty().bind(bodyTextProperty.booleanBinding{it!=null})
-            }
-            label(bodyTextProperty) {
-                addClass(DefaultStyles.defaultBody)
-            }
-        }
-        label(majorLabelProperty) {
-            addClass(DefaultStyles.defaultMajorLabel)
-        }
-        label(minorLabelProperty) {
-            addClass(DefaultStyles.defaultMinorLabel)
-        }
-        progressbar(0.2) { addClass(DefaultStyles.defaultCardProgressBar) }
-        addClass(DefaultStyles.defaultInnerCard)
     }
 
 }
 
-fun innercard(init: InnerCard.() -> Unit = {}): InnerCard {
-    val ic = InnerCard()
+fun innercard(cardGraphic: Node? = null, init: InnerCard.() -> Unit = {}): InnerCard {
+    val ic = InnerCard(cardGraphic)
     ic.init()
     return ic
 }
