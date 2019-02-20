2d79bfd695b6b7d8bb9ebf1f18f735fb4346239a
KJoslyn <KJoslyn29@gmail.com>
2019-02-20 16:45:45 -0500

    Polished and moved ZipEntryTreeBuilder to new package

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/inject/Injector.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/ui/inject/Injector.kt
@@ -1,6 +1,6 @@
 package org.wycliffeassociates.otter.jvm.app.ui.inject
 
-import org.wycliffeassociates.otter.jvm.ZipEntryTreeBuilder
+import org.wycliffeassociates.otter.jvm.domain.resourceontainer.project.ZipEntryTreeBuilder
 import org.wycliffeassociates.otter.jvm.device.audio.injection.DaggerAudioComponent
 import org.wycliffeassociates.otter.jvm.device.audioplugin.injection.DaggerAudioPluginComponent
 import org.wycliffeassociates.otter.jvm.persistence.injection.DaggerPersistenceComponent
