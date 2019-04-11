7d11aced97e79ddc417730786f0328cfea592510
Kevin Joslyn <KJoslyn29@gmail.com>
2019-04-11 09:45:51 -0400

    Removed unused additions

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/theme/AppColors.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/theme/AppColors.kt
@@ -8,7 +8,6 @@ abstract class AppColors {
     val appRed: Color = c("#CC4141")
     val appBlue: Color = c("#0094F0")
     val appGreen: Color = c("#58BD2F")
-    val appOrange: Color = c("#F7971E")
 
     abstract val base: Color
     abstract val defaultBackground: Color
