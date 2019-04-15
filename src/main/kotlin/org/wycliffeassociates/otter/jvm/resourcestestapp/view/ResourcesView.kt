8a149a9836cba5bf8299a491e36040a2a2bf2f05
Kevin Joslyn <KJoslyn29@gmail.com>
2019-04-15 18:11:33 -0400

    Test app for resource take cards

---
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/resourcestestapp/view/ResourcesView.kt
@@ -0,0 +1,22 @@
+package org.wycliffeassociates.otter.jvm.resourcestestapp.view
+
+import org.wycliffeassociates.otter.jvm.app.ui.resourcetakes.view.ResourceTakesView
+import tornadofx.View
+import tornadofx.Workspace
+import tornadofx.removeFromParent
+import tornadofx.vbox
+
+class ResourcesView : View() {
+
+    override val root = vbox {}
+
+    var activeFragment: Workspace = Workspace()
+
+    init {
+        activeFragment.header.removeFromParent()
+
+//        activeFragment.dock<ResourceListFragment>()
+        activeFragment.dock<ResourceTakesView>()
+        add(activeFragment)
+    }
+}
