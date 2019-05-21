c112452767cb8d1d91e014b55a6fe5664d6ee46d
Kevin Joslyn <KJoslyn29@gmail.com>
2019-05-21 15:46:54 -0400

    PR Comment: Kill progressProperty subscription

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/app/widgets/resourcecard/model/ResourceCardItem.kt
+++ src/main/kotlin/org/wycliffeassociates/otter/jvm/app/widgets/resourcecard/model/ResourceCardItem.kt
@@ -1,5 +1,6 @@
 package org.wycliffeassociates.otter.jvm.app.widgets.resourcecard.model
 
+import io.reactivex.disposables.CompositeDisposable
 import javafx.beans.property.DoubleProperty
 import javafx.beans.property.SimpleDoubleProperty
 import org.wycliffeassociates.otter.common.data.workbook.AssociatedAudio
@@ -9,15 +10,21 @@ import org.commonmark.renderer.text.TextContentRenderer
 
 data class ResourceCardItem(val resource: Resource, val onSelect: () -> Unit) {
     val title: String = getTitleTextContent()
+    private val disposables = CompositeDisposable()
     val titleProgressProperty: DoubleProperty = resource.titleAudio.progressProperty()
     val bodyProgressProperty: DoubleProperty? = resource.bodyAudio?.progressProperty()
     val hasBodyAudio: Boolean = resource.bodyAudio != null
 
+    fun clearDisposables() {
+        disposables.clear()
+    }
+
     private fun AssociatedAudio.progressProperty(): DoubleProperty {
         val progressProperty = SimpleDoubleProperty(0.0)
-        this.selected.subscribe {
+        val sub = this.selected.subscribe {
             progressProperty.set( if (it.value != null) 1.0 else 0.0)
         }
+        disposables.add(sub)
         return progressProperty
     }
 
