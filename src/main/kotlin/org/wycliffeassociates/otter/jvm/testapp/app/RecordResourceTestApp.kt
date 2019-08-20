1184bbf4277eb4d383d495c1b5485e66698ea83f
Kevin Joslyn <joslynk@wa.wycliffe.org>
2019-08-20 10:27:42 -0400

    Revert "Entry points for both scripture and resource takes"
    This reverts commit bd83dcdc1ff8daec21cd15b228832bfd10cd1dd2.

--- src/main/kotlin/org/wycliffeassociates/otter/jvm/testapp/app/RecordResourceTestApp.kt
+++
@@ -1,9 +0,0 @@
-package org.wycliffeassociates.otter.jvm.testapp.app
-
-import tornadofx.*
-
-class RecordResourceTestApp: App(RecordResourceTestView::class) {
-}
-fun main(args: Array<String>) {
-    launch<RecordResourceTestApp>(args)
-}
--- src/main/kotlin/org/wycliffeassociates/otter/jvm/testapp/app/RecordScriptureTestApp.kt
+++
@@ -1,9 +0,0 @@
-package org.wycliffeassociates.otter.jvm.testapp.app
-
-import tornadofx.*
-
-class RecordScriptureTestApp: App(RecordScriptureTestView::class) {
-}
-fun main(args: Array<String>) {
-    launch<RecordScriptureTestApp>(args)
-}
